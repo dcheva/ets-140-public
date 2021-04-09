44be8730f73d80f1ef8973b4f1274147c2124faf
dcheva <dmitry.cheva@gmail.com>
2021-04-09 20:16:32 +0300

    Continued from https://github.com/dcheva/ets-140-public due to repo size

---
+++ init.bat
@@ -0,0 +1,6 @@
+git init
+git add -A
+git commit -m "Continued from https://github.com/dcheva/ets-140-public due to repo size"
+git branch -M main
+git remote add origin https://github.com/dcheva/ets-140-public.git
+git push -u origin main
